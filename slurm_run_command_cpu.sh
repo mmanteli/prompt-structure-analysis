15f9c55d58403fd6125e5d937e64e54eb8e63a81
mmanteli <amanda.myntti@gmail.com>
2026-06-10 11:15:34 +0300

    slurm scripts

---
+++ slurm_run_command_cpu.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH -A project_462001394
+#SBATCH -p small
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=20G
+#SBATCH -t 00:29:59
+#SBATCH -N 1
+#SBATCH -J evaluate
+#SBATCH -o logs/%x-%j.out
+
+echo "Running on cpu: $@"
+
+# module setup
+module purge
+module use /appl/local/csc/modulefiles
+module load pytorch/2.7
+export PYTHONPATH=/scratch/project_462001394/amanda/pythonuserbase/lib/python3.11/site-packages:$PYTHONPATH
+export HF_HOME=/scratch/project_462001394/hf_cache 
+export HFKEY=$(cat ../hf_token.txt)  # for some reason this cannot be found from env?
+
+srun "$@"
